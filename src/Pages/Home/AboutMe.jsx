04c1676a2ded411637b2b3543475413457ee7c4b
akosipaeng <mr.vasquezr@gmail.com>
2023-08-20 13:36:29 +0800

    Adding aboutme section

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,21 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias rerum sequi eius rem at nihil officiis placeat, incidunt quaerat ad adipisci nemo consequatur reiciendis aut molestiae atque quisquam ipsum et?
+          </p>
+          <p className="hero--section-description">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nemo repellat, accusamus perspiciatis iusto animi deserunt doloremque? Optio repellat fuga minus! Asperiores porro incidunt, expedita quas quam quis eligendi vel veniam.wew
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
