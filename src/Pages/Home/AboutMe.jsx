7e7a726ec3d7baa3f0d2ce91ed01e853698590a2
akosipaeng <mr.vasquezr@gmail.com>
2023-08-21 12:52:55 +0800

    Code update

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
       <div className="hero--section--content--box about--section--box">
         <div className="hero--section--content">
           {/* <p className="section--title">About</p> */}
-          <h1 className="skills-section--heading">About Me</h1>
+          <h1 className="skills--section--heading">About Me</h1>
           <p className="hero--section-description"> 
           I'm Rafael, a <strong>junior web developer</strong> dedicated to crafting seamless digital solutions. Proficient in HTML, CSS, and JavaScript, I thrive on turning ideas into user-friendly websites. 
           </p>
